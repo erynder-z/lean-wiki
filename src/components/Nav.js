c6b1a11cafcb92d15044104e7b1923608823b3e0
erynder-z <stfn.bgr@gmail.com>
2022-04-23 13:51:50 +0200

    Initial setup

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <nav>
+      <h3>Something</h3>
+      <ul className="nav-links">
+        <li>Search</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+function Search() {
+  return (
+    <div className="search">
+      <h1>Search</h1>
+    </div>
+  );
+}
+
+export default Search;
