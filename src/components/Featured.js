8818b7ab854bf02bff5955555d4cd4544ed3640c
erynder-z <stfn.bgr@gmail.com>
2022-04-24 17:08:26 +0200

    Display wikipedia's featured article

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,7 +1,45 @@
+import React, { useState } from 'react';
+
 function Featured() {
+  const [article, setArticle] = useState({
+    title: '',
+    summary: '',
+  });
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+
+  const monthFormatted = month.toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+
+  async function fetchFeaturedArticle() {
+    try {
+      const response = await fetch(
+        `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${year}/${monthFormatted}/${day}`,
+      );
+
+      const fetchData = await response.json();
+
+      setArticle({
+        title: fetchData.tfa.displaytitle,
+        summary: fetchData.tfa.extract,
+      });
+    } catch (error) {
+      console.log(`There has been a problem with your fetch operation:${error}`);
+    }
+  }
+
+  fetchFeaturedArticle();
+
   return (
     <div className="featured">
-      <h1>Featured</h1>
+      <h1>Featured Article</h1>
+      <h4 className="article-title">{article.title}</h4>
+      <div className="article-body">{article.summary}</div>
     </div>
   );
 }
