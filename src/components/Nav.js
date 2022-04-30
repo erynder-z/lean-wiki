9c5bc1dde398dd771b044ec12073aba9823f1f8f
erynder-z <stfn.bgr@gmail.com>
2022-04-30 18:12:04 +0200

    Move nav styling to css file

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,24 @@
 import '../styles/Nav.css';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function Nav() {
-  const navStyle = {
-    color: 'whitesmoke',
-    textDecoration: 'none',
-  };
+function Nav(props) {
+  const { darkmode } = props;
 
   return (
-    <nav>
+    <nav className={`${darkmode === true ? 'dark' : null}`}>
       <h3>lean wiki</h3>
       <ul className="nav-links">
-        <Link style={navStyle} to="/">
-          Home
-        </Link>
-        <Link style={navStyle} to="/search">
-          Search
-        </Link>
-        <Link style={navStyle} to="/about">
-          About
-        </Link>
+        <Link to="/">Home</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/about">About</Link>
       </ul>
     </nav>
   );
 }
 
 export default Nav;
+
+Nav.propTypes = {
+  darkmode: PropTypes.bool.isRequired,
+};
