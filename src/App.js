f3f2528e2dd9e41ee74989135bbc4aff4e3060f0
erynder-z <stfn.bgr@gmail.com>
2022-04-30 17:21:46 +0200

    Remove focus from button after click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './styles/darkmode.css';
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
@@ -9,12 +10,13 @@ import About from './components/About';
 function App() {
   const [darkmode, setDarkmode] = useState(false);
 
-  const toggleDarkMode = () => {
+  const toggleDarkMode = (e) => {
     if (darkmode === true) {
       setDarkmode(false);
     } else if (darkmode === false) {
       setDarkmode(true);
     }
+    e.target.blur();
   };
 
   return (
