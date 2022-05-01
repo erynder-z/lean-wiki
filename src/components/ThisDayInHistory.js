fe1b9afdf3e0a553fb036f06bcb067ed19ddd182
erynder-z <stfn.bgr@gmail.com>
2022-05-01 10:41:36 +0200

    Format date

--- src/components/ThisDayInHistory.js
+++ src/components/ThisDayInHistory.js
@@ -14,10 +14,15 @@ function ThisDayInHistory(props) {
     useGrouping: false,
   });
 
+  const dayFormatted = day.toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+
   return (
     <div className="history-card">
       <h1>What happened on this day in history?</h1>
-      <h4>{`${monthFormatted}/${day}`}</h4>
+      <h4>{`${monthFormatted}/${dayFormatted}`}</h4>
       {onthisday.map((item) => (
         <div key={onthisday.indexOf(item).toString()} className="day-item">
           <h5 className="day-title">{item.year}</h5>
